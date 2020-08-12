9bf88cf6f35bc49dbca80305863efaa33855d981
leegeobuk <leegeobuk@gmail.com>
2020-08-13 00:57:00 +0900

    Test login

---
+++ auth/kakao/login_test.go
@@ -0,0 +1,38 @@
+package kakao
+
+import (
+	"testing"
+
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	"github.com/leegeobuk/Bible-User/model"
+)
+
+func TestLogin(t *testing.T) {
+	err := godotenv.Load()
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+
+	db, err := connectDB()
+	defer db.Close()
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+
+	tests := []struct {
+		desc string
+		user *model.KakaoUser
+		want bool
+	}{
+		{"id: 0", &model.KakaoUser{Model: gorm.Model{ID: 0}}, false},
+		{"id: 1", &model.KakaoUser{Model: gorm.Model{ID: 1}}, true},
+	}
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			if result := isValidAccount(tc.user, db); result != tc.want {
+				t.Errorf("want: %t, got: %t ", tc.want, result)
+			}
+		})
+	}
+}
