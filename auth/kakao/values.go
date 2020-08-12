66be6b7afb2ce86161920c13ebeea2bd753f0b24
leegeobuk <leegeobuk@gmail.com>
2020-08-12 18:36:05 +0900

    Removed env package

--- auth/kakao/values.go
+++ auth/kakao/values.go
@@ -4,4 +4,8 @@ import "os"
 
 var (
 	kakaoKey = os.Getenv("KAKAO_LOGIN_API_KEY")
+	dbUser   = os.Getenv("DB_USER")
+	dbPW     = os.Getenv("DB_PW")
+	dbHost   = os.Getenv("DB_HOST")
+	dbName   = os.Getenv("DB_NAME")
 )
