5f619d33445417b98145206509081b46213ce578
leegeobuk <leegeobuk@gmail.com>
2020-08-12 18:36:29 +0900

    Use gorm model struct

--- model/kakaouser.go
+++ model/kakaouser.go
@@ -1,10 +1,9 @@
 package model
 
-import "time"
+import "github.com/jinzhu/gorm"
 
 // KakaoUser is a user model for those who signed up through Kakao
 type KakaoUser struct {
-	id       int
-	nickname string
-	signedAt	time.Time
+	gorm.Model
+	Nickname string
 }
