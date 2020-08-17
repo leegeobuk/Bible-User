205f5878e51bf9cb16e366476ce28378d520397a
leegeobuk <leegeobuk@gmail.com>
2020-08-18 04:32:32 +0900

    Feat signup that checks query parameter and signup

---
+++ auth/signup.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/leegeobuk/Bible-User/auth/kakao"
+)
+
+// Signup validates user information and saves it if valid
+func Signup(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if request.QueryStringParameters["type"] == "kakao" {
+		return kakao.Signup(ctx, &request)
+	}
+
+	resp := events.APIGatewayProxyResponse{Headers: header}
+
+	return resp, nil
+}
