8ea12c500c848f4a922003d65d4cff1fe8ab5738
leegeobuk <leegeobuk@gmail.com>
2020-08-20 20:25:25 +0900

    Fix login to save refresh_token on db and cookie

--- auth/kakao/login.go
+++ auth/kakao/login.go
@@ -2,6 +2,8 @@ package kakao
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -9,7 +11,7 @@ import (
 
 // Login authenticates kakao user and decide whether to login or not
 func Login(ctx context.Context, request *events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp := events.APIGatewayProxyResponse{Headers: header}
+	resp := events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}
 
 	// get token from Kakao Login API
 	kakaoToken, err := getToken(request)
@@ -32,12 +34,40 @@ func Login(ctx context.Context, request *events.APIGatewayProxyRequest) (events.
 
 	user := kakaoUserResp.toKakaoUser()
 
-	// ok if account is a member, unauthorized  if not
+	// return token if account is a member, unauthorize  if not
 	if isMember(user, db) {
+		// copy headers due to adding cookie later
+		resp.Headers = copyHeaders(headers)
 		resp.StatusCode = http.StatusOK
-	} else {
-		resp.StatusCode = http.StatusUnauthorized
+
+		// marshal kakaoLoginResponse
+		loginResp := &kakaoLoginResponse{AccessToken: kakaoToken.AccessToken, ExpiresIn: kakaoToken.ExpiresIn - 21290}
+		data, err := json.Marshal(loginResp)
+		if err != nil {
+			return resp, err
+		}
+		resp.Body = string(data)
+
+		// set httpOnly cookie
+		cookie := &http.Cookie{Name: "refresh_token", Value: kakaoToken.RefreshToken, HttpOnly: true}
+		setCookie(resp.Headers, cookie)
+
+		// add refresh_token to the user
+		db.Model(user).UpdateColumn("refresh_token", kakaoToken.RefreshToken)
+
+		return resp, nil
 	}
+	resp.StatusCode = http.StatusUnauthorized
 
 	return resp, nil
+}
+
+func setCookie(h map[string]string, c *http.Cookie) {
+	cookieString := ""
+	if c.HttpOnly {
+		cookieString = fmt.Sprintf("%s=%s; HttpOnly", c.Name, c.Value)
+	} else {
+		cookieString = fmt.Sprintf("%s=%s;", c.Name, c.Value)
+	}
+	h["Set-Cookie"] = cookieString
 }
