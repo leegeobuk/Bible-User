c2d0aff32f04948dd7f330f8607207d48aa12547
leegeobuk <leegeobuk@gmail.com>
2020-08-20 20:26:31 +0900

    Add refreshToken field

--- model/kakaouser.go
+++ model/kakaouser.go
@@ -5,5 +5,6 @@ import "github.com/jinzhu/gorm"
 // KakaoUser is a user model for those who signed up through Kakao
 type KakaoUser struct {
 	gorm.Model
-	Nickname string
+	Nickname     string
+	RefreshToken string
 }
