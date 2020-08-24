1db403615ed952c37d9a5252a29415bd76ae467d
leegeobuk <leegeobuk@gmail.com>
2020-08-25 00:26:14 +0900

    Refactor user model

--- auth/kakao/login_test.go
+++ auth/kakao/login_test.go
@@ -22,16 +22,16 @@ func TestLogin(t *testing.T) {
 
 	tests := []struct {
 		desc string
-		user *model.KakaoUser
+		user *model.User
 		want bool
 	}{
-		{"id: 0", &model.KakaoUser{Model: gorm.Model{ID: 0}}, false},
-		{"id: 1", &model.KakaoUser{Model: gorm.Model{ID: 1}}, true},
+		{"id: 0", &model.User{Model: gorm.Model{ID: 0}}, false},
+		{"id: 1", &model.User{Model: gorm.Model{ID: 1}}, true},
 	}
-	for _, tc := range tests {
-		t.Run(tc.desc, func(t *testing.T) {
-			if result := isMember(tc.user, db); result != tc.want {
-				t.Errorf("want: %t, got: %t ", tc.want, result)
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			if result := isMember(test.user, db); result != test.want {
+				t.Errorf("want: %t, got: %t ", test.want, result)
 			}
 		})
 	}
--- model/user.go
+++ model/user.go
@@ -2,9 +2,10 @@ package model
 
 import "github.com/jinzhu/gorm"
 
-// KakaoUser is a user model for those who signed up through Kakao
-type KakaoUser struct {
+// User is a user model for those who signed up through Kakao
+type User struct {
 	gorm.Model
 	Nickname     string
+	Type         string
 	RefreshToken string
 }
