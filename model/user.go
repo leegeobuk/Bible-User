c906b88eae4841bf17c856ccfc2e859de02c5689
leegeobuk <leegeobuk@gmail.com>
2020-09-03 17:46:38 +0900

    Delete refresh_token field

--- model/user.go
+++ model/user.go
@@ -5,7 +5,6 @@ import "github.com/jinzhu/gorm"
 // User is a user model for those who signed up through Kakao
 type User struct {
 	gorm.Model
-	Nickname     string
-	Type         string
-	RefreshToken string
+	Nickname string
+	Type     string
 }
