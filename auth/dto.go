f860134b1bb0108cef9f34e6fcc3c8bab035c754
leegeobuk <leegeobuk@gmail.com>
2020-09-10 12:20:13 +0900

    Fix struct name, structure and function

--- auth/dto.go
+++ auth/dto.go
@@ -1,20 +1,25 @@
 package auth
 
 import (
-	"strings"
-
 	"github.com/leegeobuk/Bible-User/model"
 )
 
-type loginRequest struct {
-	Email     string `json:"email"`
-	PW        string `json:"password"`
-	ConfirmPW string `json:"confirmPassword"`
+type signupRequest struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	PW    string `json:"pw"`
 }
 
-func (r loginRequest) toUser() *model.User {
+func (r signupRequest) toUser() *model.User {
 	return &model.User{
-		Nickname: r.Email[:strings.Index(r.Email, "@")],
+		UserID:   r.Email,
+		Password: r.PW,
+		Name:     r.Name,
 		Type:     "app",
 	}
+}
+
+type loginRequest struct {
+	Email string `json:"email"`
+	PW    string `json:"pw"`
 }
